dec47045b1a55cfbe989ba9457e42b164acf0bf6
Exudias <editoo31@gmail.com>
2023-07-16 15:39:10 +0300

    Link to CSS, prepare to create page

--- src/pageLoader.js
+++ src/pageLoader.js
@@ -1,29 +1,27 @@
-import BannerImage from './image.jpg';
-
-const contentDiv = document.querySelector("div#content");
+const contentDiv = document.createElement("div");
+contentDiv.id = "content";
+document.body.appendChild(contentDiv);
 
 function createPage()
 {
-    let bannerImage = new Image();
-    bannerImage.src = BannerImage;
-    bannerImage.width = 200;
+    createNav();
+    createBody();
+    createFooter();
+}
 
-    contentDiv.appendChild(bannerImage);
+function createNav()
+{
+    console.log("Doing...");
+}
 
-    let credits = document.createElement("p");
-    credits.innerHTML = '<p>Photo by <a href="https://unsplash.com/@chrishardyphotography?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Chris Hardy</a> on <a href="https://unsplash.com/photos/I09FypgLoBs?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>';
+function createBody()
+{
+    console.log("It...");
+}
 
-    contentDiv.appendChild(credits);
-
-    let heading = document.createElement("h1");
-    heading.innerText = "La Casa Solegiatta";
-
-    contentDiv.appendChild(heading);
-
-    let flavourText = document.createElement("p");
-    flavourText.innerText = "A beautiful Italian restaurant!";
-
-    contentDiv.appendChild(flavourText);
+createFooter()
+{
+    console.log("Now...");
 }
 
 export default createPage;
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,38 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: "development",
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Page',
+    }),
+  ],
   module: {
     rules: [
-     {
-       test: /\.(png|svg|jpg|jpeg|gif)$/i,
-       type: 'asset/resource',
-     },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+      },
     ],
   },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  }
 };
