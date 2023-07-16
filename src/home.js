31cac01819b8b3628647a5d23ae9d95e15f83d0b
Exudias <editoo31@gmail.com>
2023-07-16 19:35:13 +0300

    Create home page

--- src/home.js
+++ src/home.js
@@ -4,7 +4,77 @@ function loadHome()
 
     let home = document.createElement("div");
     body.appendChild(home);
-    home.textContent = "Hello! I am a home page.";
+    
+    // Heading
+    let heading = document.createElement("h1");
+    heading.className = "body-heading";
+    heading.textContent = "La Casa Solegiatta";
+    home.appendChild(heading);
+
+    // Review panel
+    let review = document.createElement("div");
+    review.className = "review";
+
+    let reviewBody = document.createElement("p");
+    reviewBody.textContent = "A great restaurant in the heart of the city! The food is great, the service is quick, and the staff are very kind.";
+
+    let reviewer = document.createElement("p");
+    reviewer.textContent = "Anonymous";
+
+    review.appendChild(reviewBody);
+    review.appendChild(reviewer);
+
+    home.appendChild(review);
+
+    // Working hours
+    let hours = document.createElement("div");
+    hours.className = "hours";
+
+    let hoursTitle = document.createElement("h1");
+    hoursTitle.textContent = "Hours";
+
+    let hoursBody = document.createElement("ul");
+    hoursBody.appendChild(makeHoursListItem("Monday", "8am", "10pm"));
+    hoursBody.appendChild(makeHoursListItem("Tuesday", "8am", "10pm"));
+    hoursBody.appendChild(makeHoursListItem("Wednesday", "8am", "10pm"));
+    hoursBody.appendChild(makeHoursListItem("Thursday", "8am", "10pm"));
+    hoursBody.appendChild(makeHoursListItem("Friday", "8am", "10pm"));
+    hoursBody.appendChild(makeHoursListItem("Saturday", "8am", "10pm"));
+    hoursBody.appendChild(makeHoursListItem("Sunday"));
+
+    hours.appendChild(hoursTitle);
+    hours.appendChild(hoursBody);
+
+    home.appendChild(hours);
+
+    // Location
+    let location = document.createElement("div");
+    location.className = "location";
+
+    let locationTitle = document.createElement("h1");
+    locationTitle.textContent = "Location";
+
+    let locationBody = document.createElement("p");
+    locationBody.textContent = "123 George St., Georgeville, Georgia";
+
+    location.appendChild(locationTitle);
+    location.appendChild(locationBody);
+
+    home.appendChild(location);
+}
+
+function makeHoursListItem(day, start, end)
+{
+    let li = document.createElement("li");
+    if (start && end)
+    {
+        li.textContent = `${day}: ${start} - ${end}`;
+    }
+    else
+    {
+        li.textContent = `${day}: Not working`;
+    }
+    return li;
 }
 
 export default loadHome;
